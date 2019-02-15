9e0a58a5bcc040cb2a43d6ab2b86437bc0917d81
Samuel Custodio Cavaco <scavaco@laposte.net>
2019-02-15 16:24:13 +0100

    Ajout de la classe Deck

---
+++ commun/src/main/java/commun/Deck.java
@@ -0,0 +1,54 @@
+package commun;
+
+import commun.cartes.*;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+
+	private static ArrayList<Carte> carteEnJeu = new ArrayList();
+	ArrayList<Carte> deck = new ArrayList();
+
+    public Deck(int nbJoueur) throws Exception {
+
+        //3*7 = 3 joueur et 7 cartes par joueur
+
+        createCarteEnJeu(nbJoueur);
+
+        for (int i = 0; i < nbJoueur * 7; i++) {
+
+            int r = new Random().nextInt(carteEnJeu.size());
+            //Plus tard il faudra également faire un remove de la carte pour ne pas la piocher 2x
+            deck.add(carteEnJeu.get(r));
+
+        }
+
+        for (int i = 0; i < deck.size(); i++) {
+            System.out.println(i + " " + deck.get(i));
+        }
+    }
+
+	public ArrayList<Carte> getDeck() {
+		return deck;
+	}
+
+	public static void createCarteEnJeu(int nbJoueur) throws Exception {
+
+		switch (nbJoueur) {
+			case 3: {
+				carteEnJeu.add(new Bains());
+				carteEnJeu.add(new Autel());
+				carteEnJeu.add(new Theatre());
+				carteEnJeu.add(new PreteurSurGages());
+				break;
+			}
+			default: {
+				throw new Exception("Pas encore crée");
+			}
+		}
+
+	}
+
+
+}

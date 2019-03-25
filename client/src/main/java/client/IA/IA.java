eb68b71ff772b2c4f561a709b929a2bbcfec1767
Samuel <scavaco@laposte.net>
2019-03-25 16:18:12 +0100

    Création d'une stratégie par couleur closes #37

--- client/src/main/java/client/IA/IA.java
+++ client/src/main/java/client/IA/IA.java
@@ -20,14 +20,37 @@ public class IA {
         c.getMain().getCartes().remove((int) rand);
     }
 
-    public void jouerCarteBleu(){
+    public void jouerCarteParCouleur(int type){
+        int bestScore=-1;
+        int score=-1;
+        int indice=-1;
 
+        for(int i=0;i<c.getMain().getCartes().size();i++){ //parcours la main
+            if(c.getMain().getCartes().get(i).getType()==type){ // Verifie si c'est une carte de type passer en paramètre
+                score=c.getMain().getCartes().get(i).getPoint();
+                if(bestScore<score){ //verifie qu'il y ai aucune carte avec plus de points dans la main
+                    bestScore=score;
+                    indice=i;
+                }
+            }
+        }
+
+        if(bestScore!=-1){ //s'il y a eu une carte du type en parametre et avec le plus de points
+            c.playCard(c.getMain().getCartes().get(indice));
+        }else{ //joue une carte aléatoire sinon
+            jouerCarteAlea();
+        }
     }
+
 
     public void tour() {
         switch (strat){
+            case "random":
+                jouerCarteAlea();
             case "bleu" :
-                jouerCarteBleu();
+                jouerCarteParCouleur(1);
+            case "rouge" :
+                jouerCarteParCouleur(2);
         }
 
     }

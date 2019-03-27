2af88a80ecd2dff41d9ea57cfa577ac83380db9f
Mouun <sorel.nadaud@outlook.com>
2019-03-27 18:52:08 +0100

    Résolution d'un crash lorsque l'IA joue une carte aléatoire
    closes #42

--- client/src/main/java/client/IA/IA.java
+++ client/src/main/java/client/IA/IA.java
@@ -1,11 +1,15 @@
 package client.IA;
 
 import client.Client;
+import commun.cartes.Carte;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class IA {
 
-    Client c;
-    String strat;
+    private Client c;
+    private String strat;
 
     public IA(Client c, String strategie) {
         strat = strategie;
@@ -13,19 +17,19 @@ public class IA {
         c.setIA(true);
     }
 
-    //Stratégie nulle joue juste une carte au hasard
-    public void jouerCarteAlea(){
-        double rand = (Math.random() * (c.getMain().getCartes().size()));
-        c.playCard(c.getMain().getCartes().get((int) rand));
-        c.getMain().getCartes().remove((int) rand);
+    // Stratégie nulle joue juste une carte au hasard
+    private void jouerCarteAlea() {
+        if (!c.getMain().getCartes().isEmpty()) {
+            c.playCard(randomCarte(c.getMain().getCartes()));
+        }
     }
 
-    public void jouerCarteParCouleur(int type){
+    private void jouerCarteParCouleur(int type) {
         int bestScore=-1;
-        int score=-1;
+        int score;
         int indice=-1;
 
-        for(int i=0;i<c.getMain().getCartes().size();i++){ //parcours la main
+        for(int i=0;i<c.getMain().getCartes().size();i++) { //parcours la main
             if(c.getMain().getCartes().get(i).getType()==type){ // Verifie si c'est une carte de type passer en paramètre
                 score=c.getMain().getCartes().get(i).getPoint();
                 if(bestScore<score){ //verifie qu'il y ai aucune carte avec plus de points dans la main
@@ -35,13 +39,12 @@ public class IA {
             }
         }
 
-        if(bestScore!=-1){ //s'il y a eu une carte du type en parametre et avec le plus de points
+        if(bestScore!=-1) { //s'il y a eu une carte du type en parametre et avec le plus de points
             c.playCard(c.getMain().getCartes().get(indice));
-        }else{ //joue une carte aléatoire sinon
+        } else { //joue une carte aléatoire sinon
             jouerCarteAlea();
         }
     }
-
 
     public void tour() {
         switch (strat){
@@ -52,7 +55,11 @@ public class IA {
             case "rouge" :
                 jouerCarteParCouleur(2);
         }
-
+        c.setAJoue(true);
     }
 
+    private static Carte randomCarte(ArrayList<Carte> cartes) {
+        Random rand = new Random();
+        return cartes.get(rand.nextInt(cartes.size()));
+    }
 }

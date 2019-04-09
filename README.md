e6678962d32b61f14b4b15a2fdf97fd47f710017
Sörel Nadaud <sorel.nadaud@outlook.com>
2019-04-09 15:41:47 +0200

    Simplification du lancement du jeu
    Closes #46

--- README.md
+++ README.md
@@ -17,15 +17,9 @@
 - Il faut ouvrir le projet avec l'IDE de votre choix,
 - Se rendre dans le module jeu,
 - Lancer le main de la classe jeu,
-- Une fois que main été lancé, il est demandé à l'utilisateur d'entrer son nom. L'utilisateur peut alors entrer ce qu'il veut pour continuer,
-- Pour jouer seul :
-     - entrer **1** au choix entre "créer une partie" et "rejoindre une partie",
-     - entrer **3** au nombre de joueurs de la partie,
-     - entrer **2** au nombre de joueurs IA,
-     - ne rien entrer et simplement appuyer sur la touche **entrer du clavier** pour utiliser l'adresse par défaut,
-     - ne rien entrer et simplement appuyer sur la touche **entrer du clavier** pour utiliser le port par défaut,
- - La partie démarre ensuite avec 3 joueurs dont 2 IA.
- 
+- Une fois que main été lancé, il est demandé à l'utilisateur d'entrer son nom,
+- Lorsque le joueur a entré son nom, une partie à 3 joueurs avec 2 joueurs IA démarre.
+
  ## Pour jouer :
  
  - La liste des cartes de la main est affichée. Chaque carte porte un numéro également indiqué. Pour jouer la carte voulue, simplement entrer son numéro dans le terminal.
--- jeu/src/main/java/jeu/Jeu.java
+++ jeu/src/main/java/jeu/Jeu.java
@@ -3,81 +3,98 @@ package jeu;
 import client.Client;
 import serveur.Serveur;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Jeu {
 
     private static Scanner sc = new Scanner(System.in);
-    private static ArrayList<Client> clients = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Veuillez entrer votre nom :");
-        String nomJoueur = lireEntree();
-        System.out.println(nomJoueur + ", voulez vous (entrer 1 ou 2) :\n" +
-                "1) Créer une partie\n" +
-                "2) Rejoindre une partie");
+        String nomJoueur;
+        do {
+            nomJoueur = lireEntree("Veuillez entrer votre nom :");
+        } while (nomJoueur.equals(""));
+        formulaireClient(nomJoueur);
+    }
+
+    private static void formulaireClient(String nomJoueur) throws Exception {
         String urlServeur;
         int portServeur;
-        switch (lireEntree()) {
-            case "1": // cas création d'une partie
-                System.out.println("Veuillez entrer le nombre de joueur de la partie vous compris (entre 3 (maximum pour le moment) et 7) :");
-                int nbJoueurs = Integer.parseInt(lireEntree());
-                while (nbJoueurs < 3 || nbJoueurs > 7) {
-                    nbJoueurs = Integer.parseInt(lireEntree());
-                }
-                System.out.println("Veuillez entrer le nombre de joueurs non-humains (" + (nbJoueurs - 1) + " au maximum) :");
-                int nbJoueursIA = Integer.parseInt(lireEntree());
-                if (nbJoueursIA > (nbJoueurs - 1)) {
-                    nbJoueursIA = Integer.parseInt(lireEntree());
-                }
-                System.out.println("Veuillez entrer une adresse pour le serveur (sans le port, ne rien entrer pour l'adresse par défaut 127.0.0.1) :");
-                String entreeURL = lireEntree();
-                if (entreeURL.equals("")) urlServeur = "127.0.0.1";
-                else urlServeur = entreeURL;
-                System.out.println("Veuillez entrer un port pour le serveur (ne rien entrer pour le port par défaut 60001) :");
-                String entreePort = lireEntree();
-                if (entreePort.equals("")) portServeur = 60001;
-                else portServeur = Integer.parseInt(entreePort);
-                System.out.println("Création d'une partie pour " + nbJoueurs + " joueurs dont " + nbJoueursIA + " joueurs IA");
-                new Serveur(nbJoueurs, nbJoueursIA, urlServeur, portServeur);
+        int nbJoueurs = 3;
+        int nbJoueursIA = 2;
+        urlServeur = "127.0.0.1";
+        portServeur = 60001;
+        creerPartie(nbJoueurs, nbJoueursIA, urlServeur, portServeur, nomJoueur);
+
+        /*
+        Code optionnel qui permet au joueur de sélectionner différents paramètres pour la partie qui va être créée
+        (nombre de joueurs total, nombre de joueurs IA ainsi que url et port du serveur)
+         */
+//        switch (lireEntree(nomJoueur + ", voulez vous (entrer 1 ou 2) :\n" +
+//                "1) Créer une partie\n" +
+//                "2) Rejoindre une partie")) {
+//            case "1": // cas création d'une partie
+//                int nbJoueurs = Integer.parseInt(lireEntree("Veuillez entrer le nombre de joueur de la partie vous compris (entre 3 (maximum pour le moment) et 7) :"));
+//                while (nbJoueurs < 3 || nbJoueurs > 7) {
+//                    nbJoueurs = Integer.parseInt(lireEntree("Veuillez entrer le nombre de joueur de la partie vous compris (entre 3 (maximum pour le moment) et 7) :"));
+//                }
+//                int nbJoueursIA = Integer.parseInt(lireEntree("Veuillez entrer le nombre de joueurs non-humains (" + (nbJoueurs - 1) + " au maximum) :"));
+//                if (nbJoueursIA > (nbJoueurs - 1)) {
+//                    nbJoueursIA = Integer.parseInt(lireEntree("Veuillez entrer le nombre de joueurs non-humains (" + (nbJoueurs - 1) + " au maximum) :"));
+//                }
+//                String entreeURL = lireEntree("Veuillez entrer une adresse pour le serveur (sans le port, ne rien entrer pour l'adresse par défaut 127.0.0.1) :");
+//                if (entreeURL.equals("")) urlServeur = "127.0.0.1";
+//                else urlServeur = entreeURL;
+//                String entreePort = lireEntree("Veuillez entrer un port pour le serveur (ne rien entrer pour le port par défaut 60001) :");
+//                if (entreePort.equals("")) portServeur = 60001;
+//                else portServeur = Integer.parseInt(entreePort);
+//                creerPartie(nbJoueurs, nbJoueursIA, urlServeur, portServeur, nomJoueur);
+//                break;
+//            case "2": // cas rejoindre une partie
+//                formulaireRejoindrePartie(urlServeur, nomJoueur);
+//                break;
+//            default:
+//                formulaireClient(nomJoueur);
+//        }
+    }
+
+    /**
+     * Cette méthode est utilisée dans le morceau de code optionnel ci-dessus, elle permet de demander au joueur quel
+     * serveur de jeu il veut rejoindre
+     * @param urlServeur url du serveur
+     * @param nomJoueur nom du joueur
+     */
+    private static void formulaireRejoindrePartie(String urlServeur, String nomJoueur) {
+        switch (lireEntree("Veuillez renseigner une adresse (entrer 1 ou 2) :\n" +
+                "1) Adresse personnalisée (non implémenté pour le moment)\n" +
+                "2) Adresse par défaut (http://127.0.0.1:60001)")) {
+            case "1": // cas adresse personnalisée
+                /*
+                 * non implémenté pour le moment
+                 */
+                break;
+            case "2": // cas adresse par défaut
+                urlServeur = "http://127.0.0.1:60001";
                 new Client(nomJoueur, false).start();
-                for (int i = 0; i < nbJoueursIA; i++) {
-                    Thread.sleep(1000);
-                    new Client("IA" + i, true).start();
-                }
                 break;
-            case "2": // cas rejoindre une partie
-                System.out.println("Veuillez renseigner une adresse (entrer 1 ou 2) :\n" +
-                        "1) Adresse personnalisée (non implémenté pour le moment)\n" +
-                        "2) Adresse par défaut (http://127.0.0.1:60001)");
-                switch (lireEntree()) {
-                    case "1": // cas adresse personnalisée
-                        /*
-                         * non implémenté pour le moment
-                         */
-                        break;
-                    case "2": // cas adresse par défaut
-                        urlServeur = "http://127.0.0.1:60001";
-                        new Client(nomJoueur, false).start();
-                        break;
-                }
-                break;
+            default:
+                formulaireRejoindrePartie(urlServeur, nomJoueur);
         }
     }
 
-    private static String lireEntree() {
-        return sc.nextLine();
-    }
 
-    private static void genererClients(int nbClients) throws InterruptedException {
-        clients.add(new Client("Samuel", true));
-        clients.add(new Client("Filipe", true));
-        clients.add(new Client("Hugo", true));
-
-        for (Thread client : clients) {
-            client.start();
+    private static void creerPartie(int nbJoueurs, int nbJoueursIA, String urlServeur, int portServeur, String nomJoueur) throws Exception {
+        System.out.println("Création d'une partie pour " + nbJoueurs + " joueurs dont " + nbJoueursIA + " joueurs IA");
+        new Serveur(nbJoueurs, nbJoueursIA, urlServeur, portServeur);
+        new Client(nomJoueur, false).start();
+        for (int i = 0; i < nbJoueursIA; i++) {
             Thread.sleep(1000);
+            new Client("IA" + i, true).start();
         }
+    }
+
+    private static String lireEntree(String message) {
+        System.out.println(message);
+        return sc.nextLine();
     }
 }
